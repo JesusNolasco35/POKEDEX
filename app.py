25e9516e6e4a503a7ae4b8b8d7d768e205be2df7
JesusNolasco35 <24308060610037@cetis61.edu.mx>
2025-11-12 08:30:14 -0600

    2

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-
+import requests
 API = "https://pokeapi.co/api/v2/pokemon/"
 
 app = Flask(__name__)
 app.secret_key = '123456'
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
 
 
 
@@ -17,22 +18,24 @@ def search_pokemon():
 
     resp = requests.get(f"{API}{pokemon_name}")
     
+    if not pokemon_name:
+        flash('ingresa un nombre de pokemon')
+        return redirect(url for('index'))
+    
     if resp.status_code == 200:
         pokemon_data = resp.json()
         return render_template('pokemon.html', pokemon=pokemon_data)
 
 
 
-pokemon_info = {
+
+    pokemon_info = {
     'name': pokemon_name,
     'id': pokemon_data['id'],
     'habilities':[a['hability']['name'].title() for a in pokemon_data['abilities']],
     'sprite': pokemon_data['sprites']['front_default']
 }
     return render_template('pokemon.html', pokemon=pokemon_info)
-
-
-
 
 
 
